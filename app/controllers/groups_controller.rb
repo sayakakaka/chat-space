ac791d646434dbb7f52d36bf1802cef12151f7ef
sayakakaka <sayaka.1228@icloud.com>
2020-04-23 21:17:18 +0900

    4/23

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,16 +19,21 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    
+    
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
+
+    
   end
+
 
   private
   def group_params
