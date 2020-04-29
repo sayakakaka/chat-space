b7b220d98f93a00268974f90d8a4c2326e79f4bf
sayakakaka <sayaka.1228@icloud.com>
2020-04-29 12:12:02 +0900

    4/29

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'faker'
   gem "pry-rails"
+  gem "remotipart"
 end
 
 group :development do
--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,42 +1,43 @@
 $(function(){
+  
   function buildHTML(message){
     if ( message.image ) {
-      var html =
-       `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
             </div>
-            <div class="upper-message__date">
-              ${message.created_at}
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
             </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      var html =
-       `<div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
             </div>
-            <div class="upper-message__date">
-              ${message.created_at}
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
             </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
+          </div>`
+        return html;
     };
   }
   $('#new_message').on('submit', function(e){
@@ -61,7 +62,35 @@ $(function(){
     .fail(function(){
       alert('error');
     })
-  });
+  });  
 
-
+  
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+     });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+  }
 });
